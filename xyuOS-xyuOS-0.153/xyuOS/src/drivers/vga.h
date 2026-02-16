83162b93701efc0f3d06ff4cbe18d800ef4df19b
prunik-96 <168493482+prunik-96@users.noreply.github.com>
2026-02-16 02:09:56 -0500

    Delete xyuOS-xyuOS-0.153 directory

--- xyuOS-xyuOS-0.153/xyuOS/src/drivers/vga.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "../util/types.h"
-
-void vga_clear(uint8_t color);
-void vga_print(int x, int y, const char* str, uint8_t color);
-void vga_box(int x, int y, int w, int h, uint8_t color);
--- xyuOS-xyuOS-0.153/xyuOS/src/shell/shell.h
+++
@@ -1,7 +0,0 @@
-#ifndef SHELL_H
-#define SHELL_H
-
-void shell_init();
-void shell_handle_command(char* cmd);
-
-#endif
